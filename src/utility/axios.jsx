cc994cbf44716813e3e7687b8444a05735dc7f15
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-03-27 10:16:08 -0700

    last update

--- src/utility/axios.jsx
+++ src/utility/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:5000/", // api url
-  baseURL: "https://evangadi-forum-server-87nl.onrender.com/", // api url
+  baseURL: "https://evangadi-forum-server-nzfs.onrender.com/", // api url
   
 
   withCredentials: true,
